d80eabc83a8970a409a2ee9003d5c90f209b5e17
= <=>
2014-11-08 21:48:35 -0500

    asd

--- ShiftScope_Desk/src/shiftscope/controllers/Main.java
+++ ShiftScope_Desk/src/shiftscope/controllers/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static ShiftScopeWebSocket s;
     public static void main(String[] args) {
         Handlers.buildLibraryTreeFromFile();
-        s = new ShiftScopeWebSocket(URI.create("ws://127.0.0.1:8001"));
+        s = new ShiftScopeWebSocket(URI.create("ws://54.148.12.107:8001"));
         s.connect();
         ShiftScopePlayer.initPlayer();
         ViewHandler.init();
