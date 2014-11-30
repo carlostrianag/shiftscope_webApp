7b433c63ed7d4cff08bb7281dbd7190b678ee7c3
= <=>
2014-11-30 17:50:59 -0500

    stable version

--- ShiftScope/build/classes/shiftscope/util/java/org/java_websocket/server/WebSocketServer.rs
+++
@@ -1,4 +0,0 @@
-org.java_websocket.server.WebSocketServer$WebSocketWorker
-org.java_websocket.server.WebSocketServer$WebSocketServerFactory
-org.java_websocket.server.WebSocketServer
-org.java_websocket.server.WebSocketServer$WebSocketWorker$1
