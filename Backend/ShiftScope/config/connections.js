2c3e91a4f1f9caedbd63e7ce23999076051f6f70
Carlos Triana <trianag24@gmail.com>
2014-12-27 09:50:43 -0500

    Msg

--- Backend/ShiftScope/config/connections.js
+++ Backend/ShiftScope/config/connections.js
@@ -44,7 +44,7 @@ module.exports.connections = {
     adapter: 'sails-mysql',
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'medellin',
     database: 'shiftscope_db'
   },
 
