d5d2390cb96af33e864935df3e2cd7bd7d460a36
carlostrianag <trianag24@gmail.com>
2015-04-08 22:42:37 -0500

    Web page fixes, android volume styles ready

--- Webpage/js/index.js
+++ Webpage/js/index.js
@@ -9,21 +9,33 @@ $(document).ready(function() {
 		$(item).css('padding-top', ((WINDOW_HEIGHT/2)-$(item).find('img').height()/2)+'px')
 	})
 
-	$('#happiness').css('-webkit-transform', 'translateX('+(-WINDOW_WIDTH)+'px)')
-	$('#local').css('-webkit-transform', 'translateX('+(WINDOW_WIDTH*2)+'px)')
-	$('#happiness').addClass('ease-transition')
-	$('#local').addClass('ease-transition')
+	$('#happiness img').css('-webkit-transform', 'translateX('+(-WINDOW_WIDTH)+'px)')
+	$($('#how-it-works-bars img')[0]).css('-webkit-transform', 'translateX('+(-WINDOW_WIDTH)+'px)')
+	$($('#how-it-works-bars img')[1]).css('-webkit-transform', 'translateX('+(WINDOW_WIDTH*2)+'px)')
+	$('#local img').css('-webkit-transform', 'translateX('+(WINDOW_WIDTH*2)+'px)')
+	$('#happiness img').addClass('ease-transition')
+	$('#how-it-works-bars img').addClass('ease-transition')
+	$('#local img').addClass('ease-transition')
+	$('#local img').addClass('ease-transition')
 
 	$(this).on('scroll', function() {
 		scrollTop = $('body').scrollTop()
 		percentage = scrollTop/(WINDOW_HEIGHT)
-		console.log(percentage)
 
 		if(percentage < 1) {
-			$('#happiness').css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH/2)-($(this).width()/2)/percentage)+'px, 0, 0)')
+			$('#happiness img').css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH/2)-($(this).width()/2)/percentage)+'px, 0, 0)')
 		} else if(percentage < 2) {
 			newPercentage = percentage-1
-			$('#local').css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH)-($(this).width())*newPercentage)+'px, 0, 0)')
+			$('#happiness img').css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH/2)-($(this).width()/2))+'px, 0, 0)')
+			$('#local img').css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH)-($(this).width())*newPercentage)+'px, 0, 0)')
+		} else if (percentage < 3) {
+			newPercentage = percentage-2
+			$('#local img').css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH/2)-($(this).width()/2))+'px, 0, 0)')
+			$($('#how-it-works-bars img')[0]).css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH/2)-($(this).width()/2)/newPercentage)+'px, 0, 0)')
+			$($('#how-it-works-bars img')[1]).css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH)-($(this).width())*newPercentage)+'px, 0, 0)')
+		} else {
+			$($('#how-it-works-bars img')[0]).css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH/2)-($(this).width()/2))+'px, 0, 0)')
+			$($('#how-it-works-bars img')[1]).css('-webkit-transform', 'translate3d('+((WINDOW_WIDTH)-($(this).width()))+'px, 0, 0)')			
 		}
 	})
 })
